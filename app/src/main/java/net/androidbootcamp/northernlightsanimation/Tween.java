ae576ef03f6de16bce2efe9a132c28cad8c044dc
jaweber64 <jaWeber@janets-mbp.attlocal.net>
2016-10-26 11:18:19 -0500

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/northernlightsanimation/Tween.java
@@ -0,0 +1,18 @@
+package net.androidbootcamp.northernlightsanimation;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Tween extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tween);
+        ImageView imgRotate = (ImageView) findViewById(R.id.imgTween);
+        imgRotate.startAnimation((AnimationUtils.loadAnimation(this, R.anim.rotation)));
+    }
+}
